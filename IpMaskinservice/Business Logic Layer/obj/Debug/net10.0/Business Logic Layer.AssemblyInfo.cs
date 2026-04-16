2b61994fa276560878b3394b1ac73ab489903bb6
govher <govhertm@gmail.com>
2026-04-16 10:17:44 +0200

    nyt3

--- IpMaskinservice/Business Logic Layer/obj/Debug/net10.0/Business Logic Layer.AssemblyInfo.cs
+++ IpMaskinservice/Business Logic Layer/obj/Debug/net10.0/Business Logic Layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business Logic Layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe588a29089390b7cb63af5210e9803b7911f6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3061be7a305ee4f9494dae2eba289d1f83362bb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business Logic Layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business Logic Layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
